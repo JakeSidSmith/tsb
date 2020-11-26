1ec084cf7d61377e3a74c2fa8e6ae5cbf7e38ac0
Jake 'Sid' Smith <jake@dabapps.com>
2020-11-26 19:12:55 +0000

    Reorg config and add more dev options

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,27 @@
 export interface Config {
+  // Required
   bundle: {
     inFile: string;
     outDir: string;
     publicDir?: string;
   };
+  // Base options
   indexHTML?: {
     inFile: string;
     outDir?: string;
     outputInDev?: boolean;
   };
   tsconfigPath?: string;
-  port?: string;
-  publicDir?: string;
   hashFiles?: boolean;
-  hotReload?: boolean;
   additionalFilesToParse?: readonly string[];
   env?: Record<string, unknown>;
+  // Dev server options
+  hotReload?: boolean;
+  host?: string;
+  port?: string;
+  publicDir?: string;
+  singlePageApp?: boolean;
+  headers?: Record<string, string>;
 }
 
 export type Mode = 'development' | 'production';
