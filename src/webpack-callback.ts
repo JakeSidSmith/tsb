7d227a9d9af02b5d473dc99f827368778078524a
Jake 'Sid' Smith <hello@jakesidsmith.com>
2022-10-16 13:13:48 +0100

    Format webpack-callback.ts

--- src/webpack-callback.ts
+++ src/webpack-callback.ts
@@ -3,39 +3,38 @@ import * as logger from './logger';
 
 type WebpackCallback = (error?: Error, stats?: Stats) => void;
 
-export const createWebpackCallback = (exit: boolean): WebpackCallback => (
-  error,
-  stats
-) => {
-  if (error) {
-    logger.error(error);
-  } else if (stats?.compilation.errors.length) {
-    stats.compilation.errors.forEach((compilationError) => {
-      logger.error(compilationError.message);
-      logger.error(compilationError.file);
-    });
-  }
+export const createWebpackCallback =
+  (exit: boolean): WebpackCallback =>
+  (error, stats) => {
+    if (error) {
+      logger.error(error);
+    } else if (stats?.compilation.errors.length) {
+      stats.compilation.errors.forEach((compilationError) => {
+        logger.error(compilationError.message);
+        logger.error(compilationError.file);
+      });
+    }
 
-  if (error || stats?.compilation.errors.length) {
-    logger.error('Failed to compile');
+    if (error || stats?.compilation.errors.length) {
+      logger.error('Failed to compile');
+
+      if (exit) {
+        return process.exit(1);
+      }
+    }
+
+    if (
+      typeof stats?.endTime !== 'undefined' &&
+      typeof stats?.startTime !== 'undefined'
+    ) {
+      const time = ((stats.endTime - stats.startTime) / 1000).toFixed(2);
+
+      logger.success(`Compiled in ${time} seconds`);
+    }
 
     if (exit) {
-      return process.exit(1);
+      return process.exit(0);
     }
-  }
 
-  if (
-    typeof stats?.endTime !== 'undefined' &&
-    typeof stats?.startTime !== 'undefined'
-  ) {
-    const time = ((stats.endTime - stats.startTime) / 1000).toFixed(2);
-
-    logger.success(`Compiled in ${time} seconds`);
-  }
-
-  if (exit) {
-    return process.exit(0);
-  }
-
-  return;
-};
+    return;
+  };
